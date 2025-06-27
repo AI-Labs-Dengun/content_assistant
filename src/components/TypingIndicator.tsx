29c689eee130f261c992dc20b16148045fa78326
Rafael Matias <rafamatias102@gmail.com>
2025-06-27 16:12:47 +0100

    Improved visual positioning of bot typing indicator animation

--- src/components/TypingIndicator.tsx
+++ src/components/TypingIndicator.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TypingIndicator = () => {
   return (
-    <div className="flex items-center space-x-1 p-2">
+    <div className="flex items-center justify-center space-x-1 p-2">
       <div className="flex space-x-1">
         <div className="w-2 h-2 bg-gray-900 dark:bg-white/70 rounded-full animate-bounce" style={{ animationDelay: '0ms' }}></div>
         <div className="w-2 h-2 bg-gray-900 dark:bg-white/70 rounded-full animate-bounce" style={{ animationDelay: '150ms' }}></div>
